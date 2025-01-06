3c7a008d89c6620b8a547099ff3e417a8eafc965
Dognimain <dognimain-christ.yeo@epita.fr>
2025-01-06 16:47:50 +0100

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,4 @@
+# Saisie de la température en Celsius
+celsius = float(input("Entrez la température en Celsius : "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C correspond à {fahrenheit}°F.")
