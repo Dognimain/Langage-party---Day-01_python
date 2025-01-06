bdf8ecc034c3ae886864b86b95b5431a4ecca760
Dognimain <dognimain-christ.yeo@epita.fr>
2025-01-06 16:50:41 +0100

    Create ex05.py

---
+++ ex05.py
@@ -0,0 +1,8 @@
+# Création d'une liste contenant plusieurs éléments
+L = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Affichage de la liste originale
+print("Liste originale :", L)
+
+# Affichage de la liste dans l'ordre inversé
+print("Liste inversée :", L[::-1])
